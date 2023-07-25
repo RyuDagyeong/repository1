2b4efe6b8eae6a0931bb60af359b5be669a27c9f
RyuDagyeong <ryudk5@gmail.com>
2023-07-25 14:48:41 +0900

    delete some files

--- test2 copy 2.jl
+++ test2 copy 2.jl
@@ -4,52 +4,6 @@
 using Random
 using Plots
 
-vonNeumann = [[0,-1],[0,1],[-1,0],[1,0]] #상하좌우
-L = 30
-
-σ_ = [5, 5] #생식성 
-μ_ = [5, 5] #공격성
-
-
-function simulation2_3(σ_, μ_) 
-    sn = length(σ_) #종 개수
-    G = zeros(Int64, L, L)
-    for k in 1:sn
-        G[rand(1:L),rand(1:L)] = k #시작좌표 랜덤으로 주어짐
-    end
-    pop_ = []
-    anim = @animate for t=1:1000 #반복횟수 최대 만번까지만 하기
-        alive_ = findall(.!iszero.(G)) #0이 아닌 좌표
-        push!(pop_, length(alive_))
-        if (length(unique(G)) < 3) && (0 in unique(G)) && (t>50) break end #early stop 조건으로 이용
-
-        for alive in shuffle(alive_) #suffle은 Random에 내장된 것으로, 배열의 원소 순서를 섞어줌
-                                      #-> σ, μ에 depend 된 각 종들에게 공평하게 선공할 기회가 가도록 설정한 것
-                                      #추가적으로 번식, 공격하는 세부적인 규칙은 필요하다면 추가로 설정해도 됨
-            k = G[alive] #2개 종에서 k는 0또는 1또는 2
-            if iszero(k) continue end
-
-            idx_to = alive + CartesianIndex(rand(vonNeumann)...)
-            if !all(0 .< Tuple(idx_to) .≤ L) continue end
-
-            if iszero(G[idx_to]) && (rand() < (σ_[k] / (σ_[k] + μ_[k])))
-                G[idx_to] = k
-            elseif (G[idx_to]!=k) && (rand() ≥ (σ_[k] / (σ_[k] + μ_[k])))
-                G[idx_to] = 0 #G[idx_to]!=k에서 G[idx_to]가 0인 경우는 어차피 0으로 그대로 바꾸는 것이므로 상관없음
-            end
-        end
-        #heatmap(G,clim=(0,sn))
-    end
-    a=0,b=0
-    for i=1:L, j=1:L
-        if G[i,j] == 1 a+=1
-        elseif G[i,j] == 2 b+=1
-        end
-    end
-    return a,b
-    #gif(anim)
-end
-
 
 a,b=simulation2_3(σ_, μ_)
 
